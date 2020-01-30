16f73040149fde251ce24df037c0c9ab955d98a2
Nicholas Mullen <nwmullen@gmail.com>
2020-01-30 00:02:14 -0500

    Toggles clock toggle button text

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
         <div className="toggle-clock-button">
           <button
             onClick={ () => {toggleClock(clockSeconds, setClockSeconds, isClockRunning, setIsClockRunning)} }
-          >Toggle Clock</button>
+          >{isClockRunning ? "Stop Clock" : "Start Clock"}</button>
         </div>
       </section>
     </div>
