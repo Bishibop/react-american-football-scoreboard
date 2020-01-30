1fbbf32d8fa59c70c56622926142603ada5bb786
Nicholas Mullen <nwmullen@gmail.com>
2020-01-29 23:56:32 -0500

    Working timer with clock toggle

--- src/App.js
+++ src/App.js
@@ -5,16 +5,42 @@ import BottomRow from "./BottomRow";
 import TopRow from "./TopRow";
 import ScoringButtons from "./ScoringButtons";
 
+const toggleClock = function() {
+  let interval = null;
+  let internalSeconds = 0;
+  return function(seconds,
+                  setSeconds,
+                  isClockRunning,
+                  setIsClockRunning) {
+    let internalSeconds = seconds;
+    if (isClockRunning) {
+      clearInterval(interval);
+      interval = null;
+      setIsClockRunning(false);
+    } else {
+      interval = setInterval(() => {
+        internalSeconds += 1;
+        setSeconds(internalSeconds);
+      }, 1000);
+      setIsClockRunning(true);
+    }
+  };
+}();
+
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
   const [quarter, setQuarter] = useState(1);
+  const [clockSeconds, setClockSeconds] = useState(0);
+  const [isClockRunning, setIsClockRunning] = useState(false);
 
   return (
     <div className="container">
       <section className="scoreboard">
-        <TopRow homeScore={homeScore} awayScore={awayScore} />
+        <TopRow
+          homeScore={homeScore}
+          awayScore={awayScore}
+          clockSeconds={clockSeconds}/>
         <BottomRow quarter={quarter} />
       </section>
       <section className="buttons">
@@ -32,6 +58,11 @@ function App() {
           <button
             onClick={ () => setQuarter(quarter <= 3 ? quarter + 1 : quarter - 3) }
           >Next Quarter</button>
+        </div>
+        <div className="toggle-clock-button">
+          <button
+            onClick={ () => {toggleClock(clockSeconds, setClockSeconds, isClockRunning, setIsClockRunning)} }
+          >Toggle Clock</button>
         </div>
       </section>
     </div>
--- src/TopRow.js
+++ src/TopRow.js
@@ -2,11 +2,20 @@ import React from "react";
 import "./App.css";
 import TeamScore from "./TeamScore";
 
+function clockFormat(time) {
+  return time < 10 ? '0' + time : time.toString();
+}
+
 const TopRow = (props) => {
+  function gameTime(seconds) {
+    let displaySeconds = seconds % 60;
+    let displayMinutes = Math.floor(seconds / 60);
+    return `${clockFormat(displayMinutes)}:${clockFormat(displaySeconds)}`;
+  }
   return (
     <div className="topRow">
       <TeamScore teamLocation="home" score={props.homeScore} />
-      <div className="timer">00:03</div>
+      <div className="timer">{ gameTime(props.clockSeconds) }</div>
       <TeamScore teamLocation="away" score={props.awayScore} />
     </div>
   );
